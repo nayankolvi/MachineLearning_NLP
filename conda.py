a9c6ecfd5be3700fa62161ac17615c4b5bccb832
Kolvi <kolvi@fev.io>
2023-07-08 21:52:13 +0200

    NLP changes

---
+++ conda.py
@@ -0,0 +1,4 @@
+# to activate conda and install specific libs only on the terminal
+# press play button and click on terminal
+# use the activated libs to install conda libs
+# make sure you have conda interpretor running this file
